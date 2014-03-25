eeab97206b3d647a699e38a51d9aaa7760068e26
Collin Scangarella <collinscangarella@gmail.com>
2014-03-25 19:04:41 -0400

    Added non ascii encoding capabilities.

--- src/isitonflix/ImdbParser.java
+++ src/isitonflix/ImdbParser.java
@@ -2,6 +2,10 @@ package isitonflix;
 
 import isitonflix.Award;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,13 +38,13 @@ public class ImdbParser {
 		for (String s : strings) {
 			m = category.matcher(s);
 			if (m.matches()){
-				c = m.group(2);
+				c = cleanup(m.group(2));
 				h = c;
 				continue;
 			}
 			m = honor.matcher(s);
 			if (m.matches()){
-				h = m.group(2);
+				h = cleanup(m.group(2));
 				continue;
 			}
 			m = won.matcher(s);
@@ -54,7 +58,9 @@ public class ImdbParser {
 			}
 			m = film.matcher(s);
 			if (m.matches()){
-				Movie movie = new Movie(m.group(3), m.group(2).substring(1, m.group(2).length()-2));
+				Movie movie = new Movie(cleanup(m.group(3)), 
+						cleanup(m.group(2).substring(1, m.group(2).length()-2))
+				);
 				Award award = new Award(this.place, this.year, c, h, w);
 				addAwardToList(movie, award);
 				continue;
@@ -74,5 +80,20 @@ public class ImdbParser {
 			m.addAward(a);
 			this.movies.add(m);
 		}
+	}
+	private String cleanup(String s){
+		String[] array = s.split("&#x");
+		StringBuilder sb = new StringBuilder();
+		sb.append(array[0]);
+		for (int i = 1; i < array.length; i++){
+			String[] character = array[i].split(";");
+			Integer codePoint = Integer.parseInt(character[0], 16);
+			char[] c = Character.toChars(codePoint);
+			for (int j = 0; j < c.length; j++) {
+				sb.append(c[j]);
+			}
+			sb.append(character[1]);
+		}
+		return sb.toString();
 	}
 }
